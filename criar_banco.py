5bef8354455baa4533bf14644bc9af4d3382a9c8
libanio-prog <libanio.dev@gmail.com>
2024-03-17 12:25:32 -0300

    Recriando o banco

--- criar_banco.py
+++ criar_banco.py
@@ -4,7 +4,7 @@ con = sqlite3.connect('base.db')
 cursor = con.cursor()
 
 sql = """ 
-CREATE TABLE users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE produtos (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                     name TEXT NOT NULL,
                     marca TEXT NOT NULL,
                     variacao TEXT NOT NULL)
