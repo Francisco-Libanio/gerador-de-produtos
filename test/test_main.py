7c812fdb9f671c5f2d22d551a13be23e9ad5dda5
libanio-prog <libanio.dev@gmail.com>
2024-03-21 08:04:25 -0300

    criando o primeiro teste

--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,12 @@
-def testa_geração_de_produto():
-    ...
+from main import gerador_de_produto
+from random import seed
 
-def testa_envio_de_produto():
-    ...
+
+def testa_geração_de_produto():
+    seed(1)  # Configura a semente para reproduzir os mesmos resultados
+
+    gerador = gerador_de_produto(5)  # Quantidade pequena para facilitar o teste
+
+    # Verifica se a quantidade de combinações geradas está correta
+    assert len(gerador) == 10
+
